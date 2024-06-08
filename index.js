8a600ae40239e5dc5fe3dbd19ebf94b6483e26b3
peccu <peccul@gmail.com>
2024-06-08 16:35:04 +0900

    wip broken

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const htmlparser = require('htmlparser2');
 
-function extractElements(html, tagName) {
+function extractRepeatingElements(html) {
   const elements = [];
+  let currentElement = null;
+  let previousElement = null;
+
   const parser = new htmlparser.Parser({
     onopentag(name, attribs) {
-      if (name === tagName) {
-        elements.push({ name, attribs });
+      const element = { name, attribs, children: [] };
+      if (currentElement) {
+        currentElement.children.push(element);
+      } else {
+        elements.push(element);
+      }
+      previousElement = currentElement;
+      currentElement = element;
+    },
+    ontext(text) {
+      if (currentElement) {
+        currentElement.text = (currentElement.text || '') + text;
       }
     },
     onclosetag(name) {
-      if (name === tagName) {
-        elements[elements.length - 1].closeTag = true;
+      if (currentElement && currentElement.name === name) {
+        currentElement = previousElement;
       }
     }
   }, { decodeEntities: true });
@@ -18,7 +31,47 @@ function extractElements(html, tagName) {
   parser.write(html);
   parser.end();
 
-  return elements;
+  const repeatingElements = findRepeatingElements(elements);
+  return repeatingElements;
+}
+
+function findRepeatingElements(elements) {
+  const repeatingElements = [];
+  let previousElement = null;
+
+  for (const element of elements) {
+    if (previousElement && isEquivalent(element, previousElement)) {
+      if (!repeatingElements.length || isEquivalent(element, repeatingElements[0])) {
+        repeatingElements.push(element);
+      }
+    }
+    previousElement = element;
+  }
+
+  return repeatingElements;
+}
+
+function isEquivalent(a, b) {
+  if (a.name !== b.name || a.attribs.length !== b.attribs.length) {
+    return false;
+  }
+  for (const key in a.attribs) {
+    if (a.attribs[key] !== b.attribs[key]) {
+      return false;
+    }
+  }
+  if (a.text !== b.text) {
+    return false;
+  }
+  if (a.children.length !== b.children.length) {
+    return false;
+  }
+  for (let i = 0; i < a.children.length; i++) {
+    if (!isEquivalent(a.children[i], b.children[i])) {
+      return false;
+    }
+  }
+  return true;
 }
 
 // 使用例
@@ -38,5 +91,5 @@ const html = `
 </html>
 `;
 
-const divElements = extractElements(html, 'div');
-console.log(divElements);
+const repeatingElements = extractRepeatingElements(html);
+console.log(repeatingElements);
